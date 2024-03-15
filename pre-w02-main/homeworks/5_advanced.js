f01ef5110d75278e845e956c822301eff6e0a877
shmoon <shmoon@eilab.kr>
2024-03-15 14:56:57 +0900

    pre-w02 / 마무리

--- pre-w02-main/homeworks/5_advanced.js
+++ pre-w02-main/homeworks/5_advanced.js
@@ -10,28 +10,30 @@
        */
 
       it("다른 자료형을 더하는 경우의 형변환", function () {
-        const result = 7 + "3";
-        const guess = "73";
+        const result = 7 + "3"; // String을 만나기 전까지 형변환 x 
+        const guess = "73"; // 7 + "3"
 
         expect(result).to.equal(guess);
+
+        // + 연산자의 형변환 우선순위: Boolan < Number < String  
       });
 
       it("다른 자료형을 더하는 경우의 형변환", function () {
-        const result = "7" + 3;
-        const guess = "73";
+        const result = "7" + 3; // String을 만나는 순간 형변환 
+        const guess = "73"; // "7" + "3"
 
         expect(result).to.equal(guess);
       });
 
       it("다른 자료형을 빼는 경우의 형변환", function () {
-        const result = "7" - 3;
-        const guess = 4;
+        const result = "7" - 3; // + 이외의 산술 연산자는 모든 자료형을 Number로 형변환
+        const guess = 4; // 7 - 3
 
         expect(result).to.equal(guess);
       });
 
       it("||, && 논리 연산자", function () {
-        const result = ("0" && 30) || false;
+        const result = ("0" && 30) || false; // &&은 falsy부터, || truthy 부터
         const guess = 30;
 
         expect(result).to.equal(guess);
@@ -65,10 +67,15 @@
 
       it("문자열 반복, 자르기, 붙이기", function () {
         const a = "ha".repeat(3); //hahaha
-        const b = a.split("a"); // ['h','h','h']
-        const result = b.join(":");
+        const b = a.split("a"); // ['h','h','h','']
+        const result = b.join(":"); // h:h:h: 
         const guess = 'h:h:h:';
-
+        /*
+         * 문자열에서 separator가 등장하면 해당 부분은 삭제되고 남은 문자열이 배열로 반환된다.
+         * separator가 등장하지 않거나 생략될 경우, 배열은 원본 문자열을 유일한 원소로 가진다.
+         * separator가 빈 문자열일 경우, 원본 문자열의 모든 문자를 원소로 가지는 배열로 변환된다.
+         ** separator가 원본 문자열의 처음이나 끝에 등장할 경우 반환되는 배열도 빈 문자열로 시작하거나 끝난다. **
+         */
         expect(result).to.equal(guess);
       });
 
@@ -106,11 +113,20 @@
         /*
          * 부동 소수점에 대한 내용과 함께
          * 아래 연산 결과에 대한 자세한 원리를 최대한 정확히 조사해보세요.
+         * 
+         * 컴퓨터는 모든 데이터를 이진법으로 표현한다. 
+         * 이진법을 사용해서 모든 숫자를 정확하게 표현할 수 없다. (십진법도 마찬가지)
+         * 컴퓨터에서 실수를 이진수로 표현하는 방법 중 하나가 "부동소수점 방식" (컴퓨터는 항상 효율적으로 메모리를 저장하려고함)
+         * IEEE 754 표준에 따라 대부분의 컴퓨터는 실수를 부동소수점 방식으로 표현한다. 
+         * 부동소수점 방식은 실수를 부호, 지수, 가수로 표현하는데
+         * 실수를 이진수로 변환 후 소수점 왼쪽에 한 자리 숫자만 남도록 소수점을 이동시킨다. (정규화)
+         * 이때 이동한 소수점 자릿수를 지수로 표현하고, 정규화된 숫자에서 소수점 오른쪽 부분을 가수로 표현한다.
+         * 고정 소수점 방식보다 크고 정밀한 실수 표현이 가능하다.
          */
         const a = 0.1;
         const b = 0.2;
-        console.log(0.1 + 0.2)
-        const result = a + b === 0.3; // 일치 연산자가 더 우선순위
+        console.log(0.1 + 0.2) // 0.30000000000000004
+        const result = a + b === 0.3; 
         const guess = false;
 
         expect(result).to.equal(guess);
